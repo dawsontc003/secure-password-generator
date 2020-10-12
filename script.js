19c59f93d1bf042b18903ca05853d5f8756e97f1
Thomas Dawson <dawsontc003@gmail.com>
2020-10-12 14:33:23 -0600

    Push for updated commits with generate password character functions

--- script.js
+++ script.js
@@ -1,81 +1,93 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Declaring character variables in global memory for password generator
-
-
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value = passarray;
+console.log(passarray)
 }
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// User defined password requirements with loop for correct character length requirement
-function generatePassword() {
-  var passwordLength = prompt("Enter desired character lenght between 8 - 128 character.")
-  console.log(passwordLength)
-if(passwordLength < 8 || passwordLength > 128) {
-  alert("Minimum value 8 and maximum value 128.")
-// var passwordLength = prompt("Enter desired character lenght between 8 - 128 character.")
-return generatePassword()
-}
-
 // Declairing variables for possible password characters.
 var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var low = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var speciall = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"]
- 
+var spec = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"]
+ // Declaring array variable for generated password to pass to
+var passarray = []
 
-// for (i = 0; i < speciall.length; i++){
-//   console.log(speciall[i])
-// } 
+  // Functions for creating characters allowed by user in password
+  function generateCapCase(){
+    var passChar = caps [Math.floor (math.random() * caps.length)]
+  };
 
-  var lowerLetter = confirm ("Include lower case letters?");
-  console.log(lowerLetter)
-  var capLetter = confirm("Include Capital letters?")
-  console.log(capLetter);
-  var num = confirm("Include numeric characters?")
-  var specChar = confirm("Include special characters?")
-  console.log(specChar);
+  function generateLowCase(){
+    var passChar = low [Math.floor (Math.random() * low.length)]
+    console.log(generateLowCase)
+     };
 
-  if(lowerLetter === false && capLetter === false && num === false && specChar === false){
-    alert('One character type is required for password to generate.');
-    return generatePassword()
+  function generateNumber(){
+    var passChar = num [Math.floor (Math.random() * num.length)]
+  };
+
+  function generateSpecCase(){
+    var passChar = spec [Math.floor (Math.random() * spec.length)]
   }
-};
-
-//  if(num) {
-//     console.log(Math.random());
-//     Math.round()
-//   }
-//   console.log(num);
-
-// var ran = Math.random(); 
-// Math.floor(ran)
-// console.log(ran)
 
 
+//Initiate genereate password user experience
+function generatePassword() {
+  // Initial prompt for user to select password length
+  var passwordLength = prompt("Enter desired character lenght between 8 - 128 character.")
+  // console.log(passwordLength)
 
+  // User entry validation for desired character length selection
+if(passwordLength < 8 || passwordLength > 128) {
+  alert("Minimum value 8 and maximum value 128")
+return generatePassword()
+}
 
-// Math.random();
+  // Confirmation for character type selections from user
+  var lowerLetter = confirm ("Include lower case letters?");
+  // console.log(lowerLetter)
+  var capLetter = confirm("Include Capital letters?")
+  // console.log(capLetter);
+  var num = confirm("Include numeric characters?")
+  // console.log(num);
+  var specChar = confirm("Include special characters?")
+  // console.log(specChar);
+  // Validation to require user to select at least one character option
+  if(lowerLetter === false && capLetter === false && num === false && specChar === false){
+    alert('Atleast one character type is required for password to generate.');
+    return generatePassword()
+  };
 
-// lowercase characters boolean
-// var lowerLetter = confirm ("Include lower case letters?");
-// var capLetter = confirm("Include Capital letters?")
+  // Generate lower letter character for password
+  if (lowerLetter) {
+  passarray.push(generateLowCase)
+  }
+   // Generate capital letter character for password
+  if (capLetter) {
+    passarray.push(generateCapCase);
+  }
+   // Generate numeric character for password
+  if (num) {
+    passarray.push(generateNumber);
+  }
+    // Generate special character for password
+  if (specChar) {
+    passarray.push(generateSpecCase)
+  }
+    // forloop for generating a password with user input for character type and length
+  for (var i = 0; i < passwordLength; i++) {
+    var randomFunction = passarray[Math.floor(Math.random() * passarray.length)]
+    console.log(randomFunction)
+  }
 
-
+}
